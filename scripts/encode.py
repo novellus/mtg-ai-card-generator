159078a30dc52ed24010a5c6e27cafd85e3b1167
novellus <novellus.cato@gmail.com>
2022-12-10 23:06:02 -0800

    progress towards unreversable_modifications

--- scripts/encode.py
+++ scripts/encode.py
@@ -1,10 +1,14 @@
 import argparse
+import copy
 import json
+import pprint
+import re
+import titlecase
 
 from collections import defaultdict
 
 
-# internally formatted cards have the following fields. All fields are always present except those indicated as optional
+# internally formatted cards have the following fields. Fields are always present except those indicated as optional
 # {
 #     'a_side'          : internal format, optional, links to parent, present only on b-, c-, d-, and e- side cards
 #     'b_side'          : internal format, optional, present only on a-side (top-level) cards
@@ -12,18 +16,18 @@ from collections import defaultdict
 #     'cost'            : str or None
 #     'd_side'          : internal format, optional, present only on a-side (top-level) cards
 #     'e_side'          : internal format, optional, present only on a-side (top-level) cards
+#     'flavor'          : str or None
 #     'json_fields'     : dict, optional, only exists if the card was converted from json,
 #                         contains select unmodified fields used in decision making (eg which cards should be encoded)
-#     'loyalty'         : int or None
+#     'loyalty'         : str or None
 #     'main_text'       : str
 #     'maintypes'       : list of str (possibly empty)
 #     'name'            : str
 #     'num_sides'       : int (1-5)
-#     'power_toughness' : 2-list of int or None
+#     'power_toughness' : 2-list of str or None
 #     'rarity'          : str
 #     'subtypes'        : list of str (possibly empty)
 #     'supertypes'      : list of str (possibly empty)
-#     'unparsed_name'   : str
 # }
 
 
@@ -56,8 +60,8 @@ def json_to_internal_format(json_path):
     for k_set, v_set in list(j['data'].items()):
         # collect set cards first, to make correct b-side associations
         # then add these to the aggregate set above
-        primary_sides = defaultdict(list)
-        non_primary_sides = defaultdict(list)  # side lettering goes up to 'e', so they are not all 'b' sides
+        primary_sides = []
+        non_primary_sides = []
 
         # this is a big and complicated dataset, so lets make sure the list of available information matches our expectations
         expected_keys = ['baseSetSize', 'cards', 'code', 'isFoilOnly', 'isOnlineOnly', 'keyruneCode', 'name', 'releaseDate', 'tokens', 'totalSetSize',
@@ -100,15 +104,14 @@ def json_to_internal_format(json_path):
             else:
                 card['name'] = j_card['name']
 
-            # main_text
-            if 'isTextless' in j_card and j_card['isTextless']:
-                card['main_text'] = None
+            # main_text. json property 'isTextless' is sometimes not correct, so don't use it
+            if 'text' in j_card:
+                card['main_text'] = j_card['text']
+            elif 'originalText' in j_card:
+                card['main_text'] = j_card['originalText']
             else:
-                assert 'text' in j_card or 'originalText' in j_card
-                if 'text' in j_card:
-                    card['main_text'] = j_card['text']
-                else:
-                    card['main_text'] = j_card['originalText']
+                card['main_text'] = None
+            card['main_text'] = card['main_text'] or None  # handle empty strings, if any
 
             # cost
             if 'manaCost' in j_card:
@@ -120,16 +123,22 @@ def json_to_internal_format(json_path):
             assert (('power' in j_card and 'toughness' in j_card)
                  or ('power' not in j_card and 'toughness' not in j_card))
             if 'power' in j_card:
-                card['power_toughness'] = [int(j_card['power'],) 
-                                           int(j_card['toughness'])]
+                card['power_toughness'] = [j_card['power'],
+                                           j_card['toughness']]
             else:
                 card['power_toughness'] = None
 
             # loyalty
             if 'loyalty' in j_card:
-                card['loyalty'] = int(j_card['loyalty'])
+                card['loyalty'] = j_card['loyalty']
             else:
                 card['loyalty'] = None
+
+            # flavor
+            if 'flavorText' in j_card:
+                card['flavor'] = j_card['flavorText']
+            else:
+                card['flavor'] = None
             
             # rarity
             card['rarity'] = j_card['rarity']
@@ -149,11 +158,11 @@ def json_to_internal_format(json_path):
         # assign non_primary_sides to their primary side
         for np_card in non_primary_sides:
             for p_card in primary_sides:
-                if p_card['uuid'] in np_card['otherFaceIds']:
-                    p_card[np_card[side] + '_side'] = np_card
+                if p_card['json_fields']['uuid'] in np_card['json_fields']['otherFaceIds']:
+                    p_card[np_card['json_fields']['side'] + '_side'] = np_card
                     break
             else:
-                raise ValueError(f'Primary card not found for "{np_card['name']}" {np_card['uuid']}')
+                raise ValueError(f"Primary card not found for \"{np_card['name']}\" {np_card['json_fields']['uuid']}")
 
         # populate num_sides field
         for card in primary_sides:
@@ -189,7 +198,19 @@ def internal_format_to_human_readable(cards, out_path):
 
 def internal_format_to_AI_format(card):
     # consumes a single internal format, produces a single AI formatted string
-    pass  # TODO
+    # TODO
+
+    s = s.replace('-', dash_marker)
+    name_val = name_val.lower()
+
+    text_val = src_json['text'].lower()
+    text_val = transforms.text_pass_2_cardname(text_val, name_orig)
+    text_val = transforms.text_pass_3_unary(text_val)
+    text_val = transforms.text_pass_4a_dashes(text_val)
+    text_val = transforms.text_pass_5_counters(text_val)
+    text_val = transforms.text_pass_7_choice(text_val)
+    text_val = transforms.text_pass_9_newlines(text_val)
+    text_val = transforms.text_pass_10_symbols(text_val)
 
 
 def limit_to_AI_fields(card):
@@ -205,6 +226,8 @@ def validate(card):
     # check that X has a definition if it is present anywhere
 
     # check that counters have a type definition (or are generic counters allowed?)
+
+    # check that the only numbers that exist are 
     pass  # TODO
 
 
@@ -217,10 +240,100 @@ def error_correct_AI(AI_string):
 def unreversable_modifications(card):
     # consumes a single internal format, produces a single internal format
     # makes changes which are not reversable by AI_to_internal_format
-    # this function will return the dataset which can be directly compared to the dual_processed format for validity
     #   such as standardizing order of keywords
+    # this function will return a dataset which can be directly compared to the dual_processed format for validity
     # since the changes made by this function are not validated by reversion, they should be reviewed by hand
-    pass  # TODO
+    # TODO
+
+    card = copy.deepcopy(card)
+
+    # strip text fields
+    card['name'] = card['name'].strip()
+    if card['main_text'] is not None:
+        card['main_text'] = card['main_text'].strip()
+    if card['flavor'] is not None:
+        card['main_text'] = card['main_text'].strip()
+
+    # fix alchemy symbol prepended to card names
+    card['name'] = re.sub(r'^A-', '', card['name'])
+
+    # convert one-off large nubmers to strings, since numbers are reserved characters
+    # normal (smaller) numbers will be converted to unary, but that doesn't make sense for these
+    # there also aren't very many number above 20 actually used
+    def sub_large_numbers(s):
+        s = re.sub('100,?000', 'one-hundred-thousand', s)
+        s = re.sub('1,?996', 'nineteen-ninety-six', s)  # date instead of amount?
+        s = re.sub('1,?000', 'one-thousand', s)
+        s = s.replace('200', 'two-hundred')
+        s = s.replace('100', 'one-hundred')
+        s = s.replace('50', 'fifly')
+        s = s.replace('40', 'forty')
+        s = s.replace('30', 'thirty')
+        s = s.replace('25', 'twenty-five')
+        return s
+
+    card['name'] = sub_large_numbers(card['name'])
+    if card['main_text'] is not None:
+        card['main_text'] = sub_large_numbers(card['main_text'])
+
+    # convert common unicode characters to ascii, both for standardization for the AI, and to reduce vocab size
+    def sub_unicode(s):
+        # TODO check if we 
+        s = s.replace('\u2014', '-')    # unicode long dash
+        s = s.replace('\u2019', '"')    # single quote
+        s = s.replace('\u2018', '"')    # another single quote
+        s = s.replace('\u2212', '-')    # minus sign
+        s = s.replace('\xe6',   'ae')   # ae symbol
+        s = s.replace('\xfb',   'u')    # u with caret
+        s = s.replace('\xfa',   'u')    # u with accent
+        s = s.replace('\xe9',   'e')    # e with accent
+        s = s.replace('\xe1',   'a')    # a with accent
+        s = s.replace('\xe0',   'a')    # a with accent going the other way
+        s = s.replace('\xe2',   'a')    # a with caret
+        s = s.replace('\xf6',   'o')    # o with umlaut
+        s = s.replace('\xed',   'i')    # i with accent
+        s = s.replace('\u03c0', 'pi')   # pi
+        s = s.replace('\xae',   'r')    # Registered trademark as r
+        s = s.replace('\xbd',   '1/2')  # 1/2 unicode to string
+        s = s.replace('\u221e', 'inf')  # infinity
+        s = s.replace('\u2610', 'na')   # ballot box as na
+        return s
+
+    card['name'] = sub_unicode(card['name'])
+    if card['main_text'] is not None:
+        card['main_text'] = sub_unicode(card['main_text'])
+    if card['flavor'] is not None:
+        card['flavor'] = sub_unicode(card['flavor'])
+
+    if card['main_text'] is not None:
+        # remove rules (keyword explanation) text
+        card['main_text'] = re.sub(r'\(.*\)', '', card['main_text'])  # this makes a pretty big assumption, which is hard to verify...
+
+        # remove ability words, which thematically groups cards with a common functionality, but have no actual rules meaning
+        # TODO check capitalization
+        ability_words = ['Adamant', 'Addendum', 'Alliance', 'Battalion', 'Best in show', 'Bloodrush', 'Channel', 'Chroma', 'Cohort', 'Constellation',
+                         'Converge', 'Council\'s dilemma', 'Coven', 'Crash Land', 'Delirium', 'Domain', 'Eminence', 'Enrage', 'Fateful hour', 'Ferocious',
+                         'Formidable', 'Gear up', 'Gotcha!', 'Grandeur', 'Hellbent', 'Heroic', 'Imprint', 'Inspired', 'Join forces', 'Kinship', 'Landfall',
+                         'Lieutenant', 'Magecraft', 'Metalcraft', 'Morbid', 'Pack tactics', 'Parade!', 'Parley', 'Radiance', 'Raid', 'Rally', 'Revolt',
+                         'Spell mastery', 'Strive', 'Sweep', 'Tempting offer', 'Threshold', 'Underdog', 'Undergrowth', 'Will of the council',]
+        for word in ability_words:
+            card['main_text'] = re.sub(rf'{word}\s*-?\s*', '', card['main_text'])
+
+        text_val = transforms.text_pass_4b_x(text_val)
+        text_val = transforms.text_pass_6_uncast(text_val)
+        text_val = transforms.text_pass_8_equip(text_val)
+        text_val = transforms.text_pass_11_linetrans(text_val)  # standardize order of keywords
+
+    # TODO
+    # apply strict titlecasing to the card name
+    # this gives us a robust target for the AI to internal decoder, since the AI text is all lowercase
+    # apply the exact same transformation to the card name when found in the main text
+    #   since references to card title are encoded to special characters in the AI format, and we need to be able to find them later
+
+    # TODO recurse on b-e sides
+
+    return card
+
 
 
 def verify_decoder_reverses_encoder(cards_limited_standard, cards_dual_processed):
@@ -258,7 +371,9 @@ def encode_json_to_AI(json_path, out_path):
 
     # save the standardized dataset for human review
     internal_format_to_human_readable(cards_standard, 'cards_standard.yaml')
-    cards_limited_standard = limit_to_AI_fields(cards_original)
+    cards_limited_standard = []
+    for card in cards_original:
+        cards_limited_standard.append(limit_to_AI_fields(card))
     internal_format_to_human_readable(cards_limited_standard, 'cards_limited_standard.yaml')
 
     # transcribe to AI format, and save in designated location
